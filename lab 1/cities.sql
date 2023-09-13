4ec9d61737e7ca8d1e028edd4a8e48ae0b625475
Dasha <dashaddd4@gmail.com>
2023-09-13 10:06:10 +0300

    first lab

---
+++ lab 1/cities.sql
@@ -0,0 +1,29 @@
+1
+SELECT name, population
+FROM cities
+ORDER BY population ASC
+LIMIT 10 OFFSET 10;
+
+
+2
+SELECT name
+FROM cities
+ORDER BY name DESC
+LIMIT 30;
+
+
+3
+SELECT name,region,population
+FROM cities
+ORDER BY region,population DESC;
+
+
+4
+SELECT DISTINCT region
+FROM cities;
+
+
+5
+SELECT name, region
+FROM cities
+ORDER BY region DESC, name DESC;
