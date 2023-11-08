8227627f9143dc939c8ea0fd5b400e553d02e720
Dasha <dashaddd4@gmail.com>
2023-11-08 09:05:38 +0200

    6 edit

--- lab6/lab6.sql
+++ lab6/lab6.sql
@@ -10,28 +10,28 @@ JOIN regions ON cities.region = regions.uuid
 WHERE regions.name = 'Nord';
 
 3.
-CREATE TABLE metro_stations (
+CREATE TABLE stations (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   name varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   city_id int(10) unsigned NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE metro_lines (
+CREATE TABLE lines (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   name varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   city_id int(10) unsigned NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE metro_station_lines (
+CREATE TABLE station_lines (
   station_id int(10) unsigned NOT NULL,
   line_id int(10) unsigned NOT NULL,
   PRIMARY KEY (station_id, line_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-INSERT INTO metro_stations (name, city_id) VALUES
+INSERT INTO stations (name, city_id) VALUES
 ('Держпром', 1),
 ('Університет', 1),
 ('Майдан Конституції', 1),
@@ -40,13 +40,13 @@ INSERT INTO metro_stations (name, city_id) VALUES
 ('Метробудівників', 1),
 ;
 
-INSERT INTO metro_lines (name, city_id) VALUES
+INSERT INTO lines (name, city_id) VALUES
 ('Зелена', 1),
 ('Червона', 1),
 ('Синя', 1),
 ;
 
-INSERT INTO metro_station_lines (station_id, line_id) VALUES
+INSERT INTO station_lines (station_id, line_id) VALUES
 (1, 1),
 (2, 3),
 (3, 2),
