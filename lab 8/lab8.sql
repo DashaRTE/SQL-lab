0097fcf74171a72620df0f51b1db920447d6a7ed
Daria <123471933+DashaRTE@users.noreply.github.com>
2023-11-21 21:55:33 +0200

    edit lab 8

--- lab 8/lab8.sql
+++ lab 8/lab8.sql
@@ -13,27 +13,39 @@ CREATE TABLE `projects`
 
 CREATE TABLE `project_members` 
 (
-    `project_id` INT,
-    `user_id` INT,
-    PRIMARY KEY (`project_id`, `user_id`)
+    `project_id` INT UNSIGNED,
+    `user_id` INT UNSIGNED,
+    PRIMARY KEY (`project_id`, `user_id`),
+    FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
+
 CREATE TABLE `tasks`
 (
     `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(1000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `project_id` INT,
-    `user_id` INT
+    `project_id` INT UNSIGNED,
+    `author_id` INT UNSIGNED,
+    FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`),
+    FOREIGN KEY (`author_id`) REFERENCES `users`(`id`)
 );
+
 CREATE TABLE `task_assignees`
 (
-    `task_id` INT,
-    `user_id` INT,
-    PRIMARY KEY (`task_id`, `user_id`)
+    `task_id` INT UNSIGNED,
+    `user_id` INT UNSIGNED,
+    PRIMARY KEY (`task_id`, `user_id`),
+    FOREIGN KEY (`task_id`) REFERENCES `tasks`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
+
 CREATE TABLE `files` 
 (
     `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `task_id` INT,
-    `project_id` INT
+    `task_id` INT UNSIGNED,
+    `project_id` INT UNSIGNED,
+    `file_url` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`task_id`) REFERENCES `tasks`(`id`),
+    FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`)
 );
